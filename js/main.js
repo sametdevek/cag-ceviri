5682ac4c636362630bfa9a7b461d5bcdae244670
sametdevek <43046471+sametdevek@users.noreply.github.com>
2018-10-20 21:19:18 +0300

    .

--- js/main.js
+++ js/main.js
@@ -144,12 +144,6 @@ jQuery(document).ready(function( $ ) {
     });
   }, { offset: '80%'} );
 
-  // jQuery counterUp (used in Facts section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1250
-  });
-
   // Porfolio isotope and filter
   
   var portfolioIsotope = $('.portfolio-container').isotope({
@@ -173,14 +167,6 @@ jQuery(document).ready(function( $ ) {
     loop: true,
     responsive: { 0: { items: 5 }, 768: { items: 5 }, 900: { items: 8 }
     }
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
   });
 
 });
